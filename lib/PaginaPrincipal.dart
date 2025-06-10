637a3139a0cf22dca942b00079261aa966c89ebd
Hipnos20 <68162518+Hipnos20@users.noreply.github.com>
2025-06-10 14:44:00 -0500

    Update PaginaPrincipal.dart

--- lib/PaginaPrincipal.dart
+++ lib/PaginaPrincipal.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: PaginaPrincipal());
+  }
+}
+
+class PaginaPrincipal extends StatelessWidget {
+  const PaginaPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Página Principal')),
+      body: const Center(child: Text('¡Bienvenido a Localeats!')),
+    );
+  }
+}
